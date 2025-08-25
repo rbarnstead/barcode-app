5219d9ae688491f63704ce81d32a4f1d651252ff
Robert Barnstead <rbarnstead17@gmail.com>
2025-08-25 12:16:06 -0600

    added regex to parse the string and extract key-value pairs for the required keys

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,17 +6,16 @@ import Parser from './barcode-parse'
 
 function App() {
     const [result, setResult] = useState("sup");
-    const allowed = /(DAQ|DCS|DAC|DAD|DBB|DBC|DBD|DBA|DAG|DAI|DAJ|DAK|DCG|DAY|DAU|DAW|DAZ)([^A-Z]*)/g;
+    const [kvMap, setKVMap] = useState([]);
     const sample = "@�ANSI604432090102DL00410453ZA04940095DLDCA5DCBNONEDCDNONEDBA20270701DCSBARNSTEADDACRobertDADFrankVDBD20220704DBB19960701DBC1DAYBRODAU152cmDAG64HeritageLakeShoresDAIHeritagePointeDAJABDAKT1S4J6DAQ0710-69926DCF161449-822DCGCANDDENDDFNDDGNDAHDAZBRODAX070ZAZAA0LPsb@YiYgNJjdk\@nsd+:g9:h]0ZNc@,/FHEI9P<R7;q!['L++^1hgK&djr,lA6MB-RjRVjF>)c<Flg&,lZGR0!~";
-    let match;
-    const res = [];
+
     // const fetchAPI = async () => {
     //     const response = await axios.get("http://localhost:8080/api");
     //     console.log(response.data.fruits);
     // };
 
     useEffect(() => {
-        Parser(sample, setResult);
+        Parser(sample, setKVMap);
     }, [])
 
     return (
--- client/src/barcode-parse.jsx
+++ client/src/barcode-parse.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 
+// const allowed = /(DAQ|DCS|DAC|DAD|DBB|DBC|DBD|DBA|DAG|DAI|DAJ|DAK|DCG|DAY|DAU|DAW|DAZ)([^A-Z]*)/g;
+const ALL_CODES = [
+  "DCA","DCB","DCD","DBA","DCS","DAC","DAD","DBD","DBB","DBC","DAY","DAU","DAG","DAI","DAJ","DAK","DAQ","DCF","DCG","DDE","DDF","DDG","DAH","DAZ","DAX","ZAA"
+];
 const FIELD_CODES = [
     "DAQ", // License number
     "DCS", // Last name
@@ -13,7 +17,15 @@ const FIELD_CODES = [
     "DAK", // Postal Code
     "DCG", // Country
     ];
+const pattern = new RegExp(`(${ALL_CODES.join("|")})(.*?)(?=${ALL_CODES.join("|")}|$)`, "g");
 
 export default function Parser(text, updateResult) {
-    console.log(text);
+    const matches = [...text.matchAll(pattern)];
+    const filtered = matches.map(m => [m[1], m[2]]).filter(([key]) => FIELD_CODES.includes(key));
+    
+    const data = Object.fromEntries(filtered);
+    console.log(data);
+    setResult
+
 }
+
