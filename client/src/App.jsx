1e7746b533915e1a21975b722f5d9893d34b8902
Robert Barnstead <rbarnstead17@gmail.com>
2025-08-28 11:24:48 -0600

    created form and autopopulates from result

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,7 @@ import Scanner from './ocr-scanner'
 import Parser from './barcode-parse'
 
 function App() {
-    const [result, setResult] = useState("sup");
+    const [result, setResult] = useState("");
     const [AAMVA, setAAMVA] = useState(null);
     const sample = "@�ANSI604432090102DL00410453ZA04940095DLDCA5DCBNONEDCDNONEDBA20270701DCSBARNSTEADDACRobertDADFrankVDBD20220704DBB19960701DBC1DAYBRODAU152cmDAG64HeritageLakeShoresDAIHeritagePointeDAJABDAKT1S4J6DAQ0710-69926DCF161449-822DCGCANDDENDDFNDDGNDAHDAZBRODAX070ZAZAA0LPsb@YiYgNJjdk\@nsd+:g9:h]0ZNc@,/FHEI9P<R7;q!['L++^1hgK&djr,lA6MB-RjRVjF>)c<Flg&,lZGR0!~";
 
@@ -15,10 +15,11 @@ function App() {
     // };
 
     useEffect(() => {
+        if(result == "") return;
         const data = Parser(sample);
         console.log(data);
         setAAMVA(data);
-    }, [])
+    }, [result])
 
 
     return (
